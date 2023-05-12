26bb59364a4a97366188171ac61bab4632a86a69
Max Paulson <max_paulson@dpsk12.org>
2023-05-12 10:01:01 -0600

    setting env variables for account and region

--- Meadowlark-js/services/meadowlark-lambda/bin/meadowlark-lambda.ts
+++ Meadowlark-js/services/meadowlark-lambda/bin/meadowlark-lambda.ts
@@ -27,7 +27,7 @@ async function main() {
   new MeadowlarkLambdaStack(app, 'MeadowlarkLambdaStack', {
     resourcesSwaggerDefinition: resSpec as string,
     descriptorsSwaggerDefinition: descSpec as string,
-    dotenvVars
+    dotenvVars,
     /* If you don't specify 'env', this stack will be environment-agnostic.
      * Account/Region-dependent features and context lookups will not work,
      * but a single synthesized template can be deployed anywhere. */
@@ -38,6 +38,10 @@ async function main() {
 
     /* Uncomment the next line if you know exactly what Account and Region you
      * want to deploy the stack to. */
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION
+    }
     // env: { account: '123456789012', region: 'us-east-1' },
     /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
   });
