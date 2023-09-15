7f033369fa5a89a80efe440a182e532616e9efe7
Max Paulson <max_paulson@dpsk12.net>
2023-09-15 15:18:14 -0600

    Working Deployment

--- Meadowlark-js/services/meadowlark-lambda/lambda/meadowlark-lambda-functions/src/handlers/authorization/requestTokenAuthorizationHandler.ts
+++ Meadowlark-js/services/meadowlark-lambda/lambda/meadowlark-lambda-functions/src/handlers/authorization/requestTokenAuthorizationHandler.ts
@@ -3,11 +3,19 @@ import {
 } from '@edfi/meadowlark-authz-server';
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Handler, Context } from 'aws-lambda';
 import { fromRequest, respondWith } from '../../AuthorizationConverter';
-import { bootstrap } from '../../utilities/BootstrapMeadowlark';
+import bootstrap from '../../utilities/BootstrapMeadowlark';
 
 let isBootstrapped: boolean = false;
-bootstrap().then((result: boolean) => isBootstrapped = result)
+// bootstrap().then((result: boolean) => isBootstrapped = result)
 export const handler: Handler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
-  isBootstrapped = !isBootstrapped ? await bootstrap() : isBootstrapped;
+  if (!isBootstrapped) {
+    bootstrap().then((result: boolean) => isBootstrapped = result); // this takes forever, so we return 503
+    return respondWith({
+      statusCode: 503,
+      headers: {
+        'Retry-After': '300'
+      }
+    })
+  }
   return respondWith(await requestToken(fromRequest(event)));
 }
